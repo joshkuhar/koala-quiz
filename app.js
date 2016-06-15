3183b9a13d903b25c0bc38f6b9938c6726059042
Josh Kuhar <jdkuhar@gmail.com>
2016-06-15 18:41:26 -0400

    update

--- app.js
+++ app.js
@@ -8,30 +8,35 @@ var Questions = [
 	question: "About how many hours a day do koala bears sleep?",
 	explanation: "Koalas don’t move around much. They sleep almost 18 hours a day. They spend an enormous amount of time napping, so much time that they were once thought to be a type of cat.",
 	answers: ["8 hours", "12 hours", "18 hours"],
+	photo: "images/koala-smile.jpg",
 	correct: "2"
 	},			
 	{
 	question: "About how much do koala bears eat a day?",
 	explanation: "When koala bears aren’t sleeping, they’re usually eating eucalyptus leaves, about 2 and a half pounds a day. They eat quite a bit. The average koala weighs about 20 pounds (9 kilograms). That’s a lot of food for such a little creature. A lot of koalas are chubby, too.",
 	answers: ["2 and 1/2 pounds (1 kilogram)", "1 pound (1/2 kilogram)", "1/2 a pound (1/5 kilogram)"],
+	photo: "images/koala-munching.jpg",
 	correct: "0"
 	},
 	{
 	question: "Koala Bears are immune to which of the following?",
 	explanation: "Because they eat so many eucalyptus leaves, they never get sore throats. Eucalyptus leaves are the most popular ingredient in cough drops and very effective in preventing coughs and other throat ailments.",
 	answers: ["Sneezes", "Sore throats", "Heartburn"],
+	photo: "images/cough-drops.jpg",
 	correct: "1"
 	},
 	{
 	question: "How many trees does a koala bear need to be happy?",
 	explanation: "Koala bears eat so much food they need a small forest just to be happy, about 100 trees. It’s amazing how many trees one koala needs. They also like to nap in the branches.",
 	answers: ["20", "100", "200"],
+	photo: "images/koala-tree.jpg",
 	correct: "1"
 	},
 	{
 	question: "Who is the most famous Koala Bear of all time?",
 	explanation: "Blinky Bill is the most famous. Dorthy Wall wrote a book about Blinky way back in 1933. And it’s been selling ever since, at least in Australia.",
 	answers: ["Blinky Bill", "Kathy Koala", "Winnie the Pooh"],
+	photo: "images/blinky-bill.jpg",
 	correct: "0"
 	}
 ];
@@ -86,6 +91,8 @@ function insertExplanation(){
 	$("#explanation").empty();
 	$("#explanation").css("display", "block").css("background-color", "#C390D4");
   	$("#explanation").append(Questions[currentQuestion].explanation);
+  	
+  	$("#photo").append("<img id='koala' src='"+Questions[currentQuestion].photo+ "'alt='picture of a koala'>");
 }
 
 function insertAnswers(){
@@ -100,17 +107,19 @@ $("#start").click(function(event) {
       insertQuestion();
       insertAnswers();
       changeInitialDisplays();
-      
+      $("#photo").empty();
 });
 
+
+// Submit your answer
 $("#submit").click(function(event) {
       event.preventDefault();
       insertExplanation();
       submittedDisplays();
       checkAnswer();
-      
 });
 
+// Move on to the next question
 $("#next").click(function(event) {
       event.preventDefault();
       currentQuestion++;     
@@ -118,12 +127,14 @@ $("#next").click(function(event) {
 		changeInitialDisplays();
 		insertAnswers();
       	insertQuestion();
+      	$("#photo").empty();
       	questionTracker();
       } else {
       	gameOver();
       }
 });
 
+// Start the game over
 $("#do-over").click(function(event) {
       event.preventDefault();
       currentQuestion = 0; 
@@ -136,6 +147,7 @@ $("#do-over").click(function(event) {
       insertQuestion();
       insertAnswers();
       changeInitialDisplays();
+      $("#photo").empty();
  
 });
 /*
